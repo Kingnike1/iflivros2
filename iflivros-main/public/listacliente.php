b71152765bd5ea2ad71adfe93cbddfd655cdbe89
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-10-08 18:47:53 -0300

    update

--- iflivros-main/public/listacliente.php
+++ iflivros-main/public/listacliente.php
@@ -8,8 +8,6 @@ if (isset($_GET['valor'])) {
 }
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,116 +15,85 @@ if (isset($_GET['valor'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cliente</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/css/styles.css">
     <link rel="shortcut icon" href="../public/assets/download.png" type="../public/assets/image.png">
 </head>
 
-
-
 <body>
-    
+
     <?php
-        require_once './assets/header.html';
+    require_once './assets/header.html';
     ?>
-    
-    
+
     <form action="listacliente.php" method="get">
         Nome: <br>
         <input type="text" name="valor" value="<?php echo $valor; ?>" placeholder="Digite o nome ou o cpf para pesquisar"> <br><br>
-
         <input type="submit" value="Enviar">
     </form> <br>
 
+    <h2>Lista de Cliente</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>CPF</th>
+                <th>TELEFONE</th>
+                <th>EMAIL</th>
+                <th>DATA DE NASCIMENTO</th>
+                <th>APAGAR</th>
+            </tr>
+        </thead>
 
-
-
-        <h2>Lista de Cliente</h2>
-            <table>
-
-
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>NOME</th>
-                            <th>CPF</th>
-                            <th>TELEFONE</th>
-                            <th>EMAIL</th>
-                            <th>DATA DE NASCIMENTO</th>
-                            <th>APAGAR</th>
-                        </tr>
-                    </thead>
+        <tbody>
             <?php
             //pesquisa
             if (isset($_GET['valor'])) {
-                // $valor = $_GET['valor'];
-
                 require_once "../controle/conexao.php";
                 $sql = "SELECT * FROM cliente WHERE nome LIKE '%$valor%' or cpf LIKE '%$valor%'";
                 $resultados = mysqli_query($conexao, $sql);
 
                 if (mysqli_num_rows($resultados) == 0) {
-                    echo "Não foram encontrados resultados.";
+                    echo "<tr><td colspan='7'>Não foram encontrados resultados.</td></tr>";
 
-                    // require_once "../controle/conexao.php";
-
-                    $sql = "SELECT * FROM cliente ";
-
+                    // Segunda consulta sem filtro
+                    $sql = "SELECT * FROM cliente";
                     $resultados = mysqli_query($conexao, $sql);
+                }
 
-                    while ($linha = mysqli_fetch_array($resultados)) {
-                        $id = $linha['idcliente'];
-                        $nome = $linha['nome'];
-                        $cpf = $linha['cpf'];
-                        $telefone = $linha['telefone'];
-                        $email = $linha['email'];
-                        $data_de_nascimento = $linha['data_de_nascimento'];
-                        echo  "<tbody>";
-                        echo "<tr>";
-                        echo "<td>$id</td>";
-                        echo "<td>$nome</td>";
-                        echo "<td>$cpf</td>";
-                        echo "<td>$telefone</td>";
-                        echo "<td>$email</td>";
-                        echo "<td>$data_de_nascimento</td>";
-                        echo "<td><a href='../controle/deletar/deletar_cliente.php?id=$id'>Apagar</a></td>";  
-                        echo "</tbody>";
-                    }
-                    
-                } 
-                else {
-                    while ($linha = mysqli_fetch_array($resultados)) {
-                        $id = $linha['idcliente'];
-                        $nome = $linha['nome'];
-                        $cpf = $linha['cpf'];
-                        $telefone = $linha['telefone'];
-                        $email = $linha['email'];
-                        $data_de_nascimento = $linha['data_de_nascimento'];
-                        echo  "<tbody>";
-                        echo "<tr>";
-                        echo "<td>$id</td>";
-                        echo "<td>$nome</td>";
-                        echo "<td>$cpf</td>";
-                        echo "<td>$telefone</td>";
-                        echo "<td>$email</td>";
-                        echo "<td>$data_de_nascimento</td>";
-                        echo "<td><a href='../controle/deletar/deletar_cliente.php?id=$id'>Apagar</a></td>";  
-                        echo "</tbody>";
-                    }
+                while ($linha = mysqli_fetch_array($resultados)) {
+                    $id = $linha['idcliente'];
+                    $nome = $linha['nome'];
+                    $cpf = $linha['cpf'];
+                    $telefone = $linha['telefone'];
+                    $email = $linha['email'];
+                    $data_de_nascimento = $linha['data_de_nascimento'];
+
+                    echo "<tr>";
+                    echo "<td>$id</td>";
+                    echo "<td>$nome</td>";
+                    echo "<td>$cpf</td>";
+                    echo "<td>$telefone</td>";
+                    echo "<td>$email</td>";
+                    echo "<td>$data_de_nascimento</td>";
+                    echo "<td><a href='../controle/deletar/deletar_cliente.php?id=$id'>Apagar</a></td>";
+                    echo "</tr>";
                 }
             } else {
-                echo "Digite o nome ou o cpf para pesquisar";
+                echo "<tr><td colspan='7'>Digite o nome ou o cpf para pesquisar</td></tr>";
             }
             ?>
+        </tbody>
+    </table><br>
 
+    <footer>
+        <p>&copy; 2024 IF_LIVROS. Todos os direitos reservados.</p>
+    </footer>
 
-
-
-
-                </table><br>
-                <footer>
-                    <p>&copy; 2024 IF_LIVROS. Todos os direitos reservados.</p>
-                </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- iflivros-main/public/listalivros.php
+++ iflivros-main/public/listalivros.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../controle/verificar_login.php'
+require_once '../controle/verificar_login.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,89 +9,44 @@ require_once '../controle/verificar_login.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livros</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/css/styles.css">
-    <link rel="shortcut icon" href="../public/assets/download.png" type="../public/assets/image.png">
-
-
+    <link rel="shortcut icon" href="../public/assets/download.png" type="image/png">
 </head>
 
+<body>
     <?php
-    if (isset($_GET['valor'])){
+    if (isset($_GET['valor'])) {
         $valor = $_GET['valor'];
     } else {
         $valor = '';
     }
     ?>
 
-<body>
     <form action="listalivros.php" method="get">
-    Nome: <br>
-    <input type="text" name="valor" value="<?php echo $valor; ?>"><br><br>
-
-    <input type="submit" value="Enviar">
+        Nome: <br>
+        <input type="text" name="valor" value="<?php echo $valor; ?>"><br><br>
+        <input type="submit" value="Enviar">
     </form> <br><br>
 
     <?php 
     if (isset($_GET['valor'])) {
-
         require_once "../controle/conexao.php";
         $sql = "SELECT * FROM livro WHERE nome LIKE '%$valor%'";
         $resultados = mysqli_query($conexao, $sql);
 
         if (mysqli_num_rows($resultados) == 0) {
             echo "Não foram encontrados resultados.";
-        }else {
-            echo "<table border = '1'";
+        } else {
+            echo "<table border='1'>";
             echo "<tr>";
             echo "<td>ID</td>";
             echo "<td>Nome</td>";
-            echo "<td>Genero</td>";
+            echo "<td>Gênero</td>";
             echo "<td>Status</td>";
             echo "<td>Autor</td>";
-            while ($linha = mysqli_fetch_array($resultados)) {
-                $id = $linha['idlivros'];
-                $nome = $linha['nome'];
-                $genero = $linha['genero'];
-                $status = $linha['status'];
-                $autor = $linha['autor'];
-            }
-            echo "<tr>";
-            echo "<td>$id</td>";
-            echo "<td>$nome</td>";
-            echo "<td>$genero</td>";
-            echo "<td>$status</td>";
-            echo "<td>$autor</td>";
-            echo "</tr>";
+            echo "</tr>"; // Movei a linha de cabeçalho para o lugar correto
             
-        }
-    }
-    ?>
-
-    <?php
-        require_once './assets/header.html'
-    ?>
-    <h2>Lista de Livros</h2>
-    <table>
-
-
-        <table>
-            <thead>
-
-                <tr>
-                    <th>ID</th>
-                    <th>NOME</th>
-                    <th>GENERO</th>
-                    <th>STATUS</th>
-                    <th>AUTOR</th>
-                </tr>
-            </thead>
-            <?php
-            require_once "../controle/conexao.php";
-
-            $sql = "SELECT * FROM livro";
-
-            $resultados = mysqli_query($conexao, $sql);
-
             while ($linha = mysqli_fetch_array($resultados)) {
                 $id = $linha['idlivros'];
                 $nome = $linha['nome'];
@@ -104,17 +59,63 @@ require_once '../controle/verificar_login.php'
                 echo "<td>$nome</td>";
                 echo "<td>$genero</td>";
                 echo "<td>$status</td>";
-                echo "<td>
-                        <a href='../controle/deletar/delete_livros.php?id=$id'></a>
-                        </td>";               
-                 echo "</tr>";
+                echo "<td>$autor</td>";
+                echo "</tr>";
             }
-            ?>
-        </table><br>
+            echo "</table>"; // Fechei a tabela após o loop
+        }
+    }
+    ?>
 
-        <footer>
-            <p>&copy; 2024 IF_LIVROS. Todos os direitos reservados.</p>
-        </footer>
+    <?php require_once './assets/header.html'; ?>
+
+    <h2>Lista de Livros</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>GÊNERO</th>
+                <th>STATUS</th>
+                <th>AUTOR</th>
+                <th>APAGAR</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        require_once "../controle/conexao.php";
+        $sql = "SELECT * FROM livro";
+        $resultados = mysqli_query($conexao, $sql);
+
+        while ($linha = mysqli_fetch_array($resultados)) {
+            $id = $linha['idlivros'];
+            $nome = $linha['nome'];
+            $genero = $linha['genero'];
+            $status = $linha['status'];
+            $autor = $linha['autor'];
+
+            echo "<tr>";
+            echo "<td>$id</td>";
+            echo "<td>$nome</td>";
+            echo "<td>$genero</td>";
+            echo "<td>$status</td>";
+            echo "<td>$autor</td>";
+            echo "<td>
+                    <a href='../controle/deletar/delete_livros.php?id=$id' class='btn btn-danger'>APAGAR</a>
+                  </td>";               
+            echo "</tr>";
+        }
+        ?>
+        </tbody>
+    </table><br>
+
+    <footer>
+        <p>&copy; 2024 IF_LIVROS. Todos os direitos reservados.</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
