29471263d60ec06f28f6b75e5ccc21cf076d6a59
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-10-24 21:39:14 -0300

    update

--- iflivros-main/public/listacliente.php
+++ iflivros-main/public/listacliente.php
@@ -12,10 +12,10 @@ $valor = isset($_GET['valor']) ? $_GET['valor'] : '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cliente</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../public/css/header.css">
-    <link rel="stylesheet" href="../public/css/styles_form.css">
     <link rel="shortcut icon" href="../public/assets/download.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../public/css/header.css">
+    <link rel="stylesheet" href="../public/css/styles_form.css">
 </head>
 
 
