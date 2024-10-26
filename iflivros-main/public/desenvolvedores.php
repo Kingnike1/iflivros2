cfee160d20e5563ea4d0b21028c36f77455e2eea
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-10-26 20:46:39 -0300

    update

--- iflivros-main/public/desenvolvedores.php
+++ iflivros-main/public/desenvolvedores.php
@@ -24,7 +24,7 @@ require_once '../controle/verificar_login.php';
     <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
     
     <!-- Estilos personalizados -->
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/desenvolvedores.css">
     <link rel="shortcut icon" href="./assets/download.png" type="image/png">
 </head>
 <body>
@@ -32,55 +32,44 @@ require_once '../controle/verificar_login.php';
         <h2 class="text-center">Equipe de Desenvolvimento</h2>
         <div class="row mt-4">
             <div class="col-md-4 text-center" data-aos="fade-up">
-                <div class="card mb-4">
-                    <img src="./assets/dev1.jpg" class="card-img-top" alt="Desenvolvedor 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Desenvolvedor 1</h5>
-                        <p class="card-text">Nome: Pablo Rodrigues </p>
-                    </div>
+                <div class="container overlay-container">
+                    <p>Desenvolvedor 1</p>
+                    <img src="./assets/dev1.jpeg" alt="Desenvolvedor 1" class="image">
+                    <div class="overlay">Nome: Pablo Rodrigues</div>
                 </div>
             </div>
             <div class="col-md-4 text-center" data-aos="fade-up">
-                <div class="card mb-4">
-                    <img src="./assets/dev2.jpg" class="card-img-top" alt="Desenvolvedor 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Desenvolvedor 2</h5>
-                        <p class="card-text">Nome: kaio Barbosa </p>
-                    </div>
+                <div class="container overlay-container">
+                    <p>Desenvolvedor 2</p>
+                    <img src="./assets/dev3.jpeg" alt="Desenvolvedor 3" class="image">
+                    <div class="overlay">Nome: Paulo Ricardo</div>
                 </div>
             </div>
             <div class="col-md-4 text-center" data-aos="fade-up">
-                <div class="card mb-4">
-                    <img src="./assets/dev3.jpg" class="card-img-top" alt="Desenvolvedor 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Desenvolvedor 3</h5>
-                        <p class="card-text">Nome: Paulo Ricardo </p>
-                    </div>
+                <div class="container overlay-container">
+                    <p>Desenvolvedor 3</p>
+                    <img src="./assets/dev2.jpeg" alt="Desenvolvedor 2" class="image">
+                    <div class="overlay">Nome: Kaio Barbosa</div>
                 </div>
             </div>
             <div class="col-md-4 text-center" data-aos="fade-up">
-                <div class="card mb-4">
-                    <img src="./assets/dev3.jpg" class="card-img-top" alt="Desenvolvedor 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Desenvolvedor 4</h5>
-                        <p class="card-text">Nome: Bianca </p>
-                    </div>
+                <div class="container overlay-container">
+                    <p>Desenvolvedor 4</p>
+                    <img src="./assets/dev4.jpeg" alt="Desenvolvedor 4" class="image">
+                    <div class="overlay">Nome: Bianca Vitoria</div>
                 </div>
             </div>
             <div class="col-md-4 text-center" data-aos="fade-up">
-                <div class="card mb-4">
-                    <img src="./assets/dev3.jpg" class="card-img-top" alt="Desenvolvedor 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Desenvolvedor 5</h5>
-                        <p class="card-text">Nome: Sara Xavier </p>
-                    </div>
+                <div class="container overlay-container">
+                    <p>Desenvolvedor 5</p>
+                    <img src="./assets/dev5.jpeg" alt="Desenvolvedor 5" class="image">
+                    <div class="overlay">Nome: Sara Xavier</div>
                 </div>
             </div>
-        <div>
+        </div>
     </div> 
-    
-    
 
+    <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
