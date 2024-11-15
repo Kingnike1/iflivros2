24236c624e15d6590fe0a31431f01cde998ed30d
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-11-15 00:15:34 -0300

    consertando o hash do login

--- iflivros-main/controle/login.php
+++ iflivros-main/controle/login.php
@@ -7,12 +7,24 @@
     
     $resultados = mysqli_query($conexao, $sql);
 
-    if(mysqli_num_rows($resultados) == 0) {
-        header("location: ../public/index.php");
+    if (mysqli_num_rows($resultados) == 0) {
+        // usuário não cadastrado
+        // ou informou dados errados.
+        header("Location: ../public/index.php");
     } else {
-        session_start();
-        $_SESSION['logado'] = 1;
-
-        header("location: ../public/home.php");
+        $linha = mysqli_fetch_array($resultados);
+        $senhaHash = $linha['senha'];
+    
+        // verdadeiro ou falso
+        if (password_verify($senha, $senhaHash) == true) {
+            // pode acessar.
+            session_start();
+            $_SESSION['logado'] = 1;
+    
+            header("Location: ../public/home.php");
+        } else {
+            header("Location: ../public/index.php");
+        }
     }
+    
 ?>
