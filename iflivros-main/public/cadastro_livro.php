73590895d9a445b4f0c15d312227fb540300f881
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-11-14 23:31:57 -0300

    deletei o cadastro_livrocopy

--- iflivros-main/public/cadastro_livro.php
+++ iflivros-main/public/cadastro_livro.php
@@ -1,22 +1,26 @@
 <?php
 require_once "../controle/verificar_login.php";
 
-if (isset($_GET['id'])){
+if (isset($_GET['id'])) {
 
-require_once "../controle/conexao.php";
+    require_once "../controle/conexao.php";
 
-$id = $_GET['id'];
-$sql = "SELECT * FROM livro WHERE idlivros = $id";
-$resultado = mysqli_query($conexao, $sql);
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM livro WHERE idlivros = $id";
+    $resultado = mysqli_query($conexao, $sql);
 
-$linha = mysqli_fetch_array($resultado);
+    $linha = mysqli_fetch_array($resultado);
 
-$nome = $linha['nome'];
-$genero = $linha['genero'];
-$status = $linha['status'];
-$autor = $linha['autor'];
+    $nome = $linha['nome'];
+    $genero = $linha['genero'];
+    $status = $linha['status'];
+    $autor = $linha['autor'];
+    $botao = "Salvar";
 
-$botao = "Salva";
+    // Verifique o valor de status (opcional para depuração)
+    // var_dump($status); // Para ver o valor real de status
+
+    // Certifique-se de que o valor de status seja um dos esperados
 
 } else {
     $id = 0;
@@ -26,7 +30,6 @@ $botao = "Salva";
     $autor = '';
 
     $botao = "Cadastrar";
-
 }
 ?>
 
@@ -39,16 +42,16 @@ $botao = "Salva";
     <link rel="shortcut icon" href="./assets/download.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/css/header.css">
-
     <link rel="stylesheet" href="./css/style_form.css">
 </head>
 <body>
 <?php require_once './templates/header.html'; ?>
 
-    <form action="../controle/banco_livro.php? <?php echo $id ?>" method="post" class="form">
+    <form action="../controle/banco_livro.php?<?php echo $id ?>" method="post" class="form">
         <p class="title">Cadastro de Livro</p>
         <p class="message">Preencha os dados abaixo para cadastrar um novo livro.</p>
         <input type="hidden" name="id" value="<?php echo $id ?>">
+
         <!-- Nome -->
         <div class="flex">
             <label>
@@ -56,7 +59,7 @@ $botao = "Salva";
                 <span>Nome:</span>
             </label>
         </div>
-        <!-- <br /><b>Warning</b>:  Undefined variable $genero in <b>/var/www/html/public/cadastro_livro.php</b> on line <b>63</b><br /> -->
+
         <!-- Gênero -->
         <div class="flex">
             <label>
@@ -64,7 +67,7 @@ $botao = "Salva";
                 <span>Gênero:</span>
             </label>
         </div>
-        
+
         <!-- Autor -->
         <div class="flex">
             <label>
@@ -73,29 +76,26 @@ $botao = "Salva";
             </label>
         </div>
 
-        
-
         <!-- Status -->
         <div class="flex">
             <label>Status:</label><br>
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="status" id="statusDisponivel" value="Disponível" checked>
-                <label class="form-check-label" for="statusDisponivel"></label>
+                <input class="form-check-input" type="radio" name="status" id="statusDisponivel" value="Disponível" 
+                       <?php echo ($status == 'Disponível') ? 'checked' : ''; ?>>
+                <label class="form-check-label" for="statusDisponivel">Disponível</label>
             </div>
             <div class="form-check">
-                <input class="form-check-input" type="radio" name="status" id="statusIndisponivel" value="Indisponível">
+                <input class="form-check-input" type="radio" name="status" id="statusIndisponivel" value="Indisponível" 
+                       <?php echo ($status == 'Indisponível') ? 'checked' : ''; ?>>
                 <label class="form-check-label" for="statusIndisponivel">Indisponível</label>
-
             </div>
         </div>
-        
-    
-        <button class="submit">Cadastrar Livro</button>
+
+        <button class="submit"><?php echo $botao; ?> Livro</button>
     </form>
 
     <?php require_once "../public/templates/footer.html";?>
 
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
