20f8cdb887d244b6127163e9f2883feff4ae91c6
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-11-21 17:08:18 -0300

    update

--- iflivros-main/controle/banco_emprestimo.php
+++ iflivros-main/controle/banco_emprestimo.php
@@ -1,5 +1,5 @@
 <?php
-$id = isset($_POST['id']) ? $_POST['id'] : 0;  // Verifica se id está definido
+$id = isset($_POST['id']) ? $_POST['id'] : 0;
 
 $livro_id = $_POST["livro_id"];
 $cliente_id = $_POST["cliente_id"];
@@ -9,17 +9,25 @@ $data_devolucao = $_POST["data_de_devolucao"];
 
 require_once "conexao.php";
 
-// Lógica de inserção ou atualização
 if ($id == 0) {
-    // Inserção de novo empréstimo
-    $sql = "INSERT INTO emprestimo (data_de_devolucao, data_de_emprestimo, funcionario_idfuncionario, livro_idlivros, cliente_idcliente)
-            VALUES ('$data_devolucao', '$data_emprestimo', '$funcionario_id', '$livro_id', '$cliente_id')";
+    $sql = "INSERT INTO emprestimo (data_de_devolucao, 
+                data_de_emprestimo, 
+                funcionario_idfuncionario, 
+                livro_idlivros, 
+                cliente_idcliente)
+            VALUES ('$data_devolucao', 
+                    '$data_emprestimo', 
+                    '$funcionario_id', 
+                    '$livro_id', 
+                    '$cliente_id')";
 } else {
-    // Atualização de empréstimo existente
     $sql = "UPDATE emprestimo 
-            SET livro_idlivros = '$livro_id', cliente_idcliente = '$cliente_id', funcionario_idfuncionario = '$funcionario_id', 
-                data_de_emprestimo = '$data_emprestimo', data_de_devolucao = '$data_devolucao' 
-            WHERE emprestimo = $id";  // A chave primária da tabela é 'emprestimo', não 'idfuncionario'
+            SET livro_idlivros = '$livro_id', 
+            cliente_idcliente = '$cliente_id', 
+            funcionario_idfuncionario = '$funcionario_id', 
+            data_de_emprestimo = '$data_emprestimo',
+            data_de_devolucao = '$data_devolucao' 
+            WHERE emprestimo = $id";
 }
 mysqli_query($conexao, $sql);
 
