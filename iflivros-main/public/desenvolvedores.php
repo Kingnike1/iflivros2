96e7640b3113f0e6bed113e375bf2456d9176099
Kingnike1 <pablo.rodrigues1@estudante.ifgoiano.edu.br>
2024-11-23 20:15:20 -0300

    update

--- iflivros-main/public/desenvolvedores.php
+++ iflivros-main/public/desenvolvedores.php
@@ -3,31 +3,33 @@ require_once '../controle/verificar_login.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desenvolvedores</title>
-    
+
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    
+
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
-    
+
     <!-- Animate.css -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
-    
+
     <!-- AOS (Animate on Scroll) -->
     <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
-    
+
     <!-- Estilos personalizados -->
     <link rel="stylesheet" href="./css/desenvolvedores.css">
     <link rel="shortcut icon" href="./assets/download.png" type="image/png">
 </head>
+
 <body>
     <div class="container mt-5">
         <!-- Seção de Introdução -->
@@ -38,10 +40,12 @@ require_once '../controle/verificar_login.php';
 
         <h2 class="text-center titulo">Equipe de Desenvolvimento</h2>
         <div class="row mt-4">
-            
+
             <div class="col-md-4 text-center" data-aos="fade-up">
                 <div class="container overlay-container">
-                    <p>Desenvolvedor 1 <br> <pre>vulgo: Sonequinha</pre></p>
+                    <p>Desenvolvedor 1 <br>
+                    <pre>vulgo: Sonequinha</pre>
+                    </p>
                     <img src="./assets/dev1.jpeg" alt="Desenvolvedor 1" class="image">
                     <div class="overlay">
                         <a href="https://github.com/Kingnike1/" target="_blank" class="link_instagram">Nome: Pablo Rodrigues</a>
@@ -51,45 +55,18 @@ require_once '../controle/verificar_login.php';
                     </div>
                 </div>
             </div>
-            
+
             <div class="col-md-4 text-center" data-aos="fade-up">
                 <div class="container overlay-container">
-                    <p>Desenvolvedor 2 <br><pre>vulgo: Cheirador de pó</pre> </p> 
-                    
+                    <p>Desenvolvedor 2 <br>
+                    <pre>vulgo: Cheirador de pó</pre>
+                    </p>
+
                     <img src="./assets/dev3.jpeg" alt="Desenvolvedor 2" class="image">
                     <div class="overlay">
                         <a href="https://github.com/Paulin09k2" class="link_instagram">Nome: Paulo Ricardo</a>
                         <p class="codinome">
                             <i class="fas fa-code-branch"></i> Back-end
-                        </p>                  
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-md-4 text-center" data-aos="fade-up">
-                <div class="container overlay-container">
-                    <p>Desenvolvedor 3 <br> <pre>vulgo: Gordão da XJ</pre></p> 
-                    
-                    <img src="./assets/dev2.jpeg" alt="Desenvolvedor 3" class="image">
-                    <div class="overlay">
-                        <a href="https://github.com/kaio062/" target="_blank" class="link_instagram">Nome: Kaio Barbosa</a>
-                        <p class="codinome">                        
-                            <i class="fas fa-tools"></i> Back-end
-                        </p>
-                    </div>                
-                </div>
-            </div>
-            
-            <div class="col-md-4 text-center" data-aos="fade-up">
-                <div class="container overlay-container">
-                <p>Desenvolvedor 2 <br><pre>vulgo: Cheirador de pó</pre> </p> 
-                    
-                    <img src="./assets/dev4.jpeg" alt="Desenvolvedor 4" class="image">
-                    <div class="overlay">
-                        <a href="https://github.com/bianckkj" class="link_instagram">Nome: Bianca Vitoria</a>
-                        <p class="codinome">
-                        <i class="fas fa-cogs"></i> Back-end
-    
                         </p>
                     </div>
                 </div>
@@ -97,29 +74,65 @@ require_once '../controle/verificar_login.php';
 
             <div class="col-md-4 text-center" data-aos="fade-up">
                 <div class="container overlay-container">
-                <p>Desenvolvedor 5 <br><pre>vulgo: Nossa tampinha</pre> </p> 
-                    
+                    <p>Desenvolvedor 3 <br>
+                    <pre>vulgo: Gordão da XJ</pre>
+                    </p>
+
+                    <img src="./assets/dev2.jpeg" alt="Desenvolvedor 3" class="image">
+                    <div class="overlay">
+                        <a href="https://github.com/kaio062/" target="_blank" class="link_instagram">Nome: Kaio Barbosa</a>
+                        <p class="codinome">
+                            <i class="fas fa-tools"></i> Back-end
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 text-center" data-aos="fade-up">
+                <div class="container overlay-container">
+                    <p>Desenvolvedor 4 <br>
+                    <pre>vulgo: Flor</pre>
+                    </p>
+
+                    <img src="./assets/dev4.jpeg" alt="Desenvolvedor 4" class="image">
+                    <div class="overlay">
+                        <a href="https://github.com/bianckkj" class="link_instagram">Nome: Bianca Vitoria</a>
+                        <p class="codinome">
+                            <i class="fas fa-cogs"></i> Back-end
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 text-center" data-aos="fade-up">
+                <div class="container overlay-container">
+                    <p>Desenvolvedor 5 <br>
+                    <pre>vulgo: Nossa tampinha</pre>
+                    </p>
+
                     <img src="./assets/dev5.jpeg" alt="Desenvolvedor 5" class="image">
                     <div class="overlay">
                         <a href="https://github.com/saraxavi" class="link_instagram">Nome: Sara Xavier</a>
                         <p class="codinome">
-                        <i class="fas fa-server"></i> Back-end
+                            <i class="fas fa-server"></i> Back-end
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
-    
+
     <script>
         // Inicializa AOS (Animate on Scroll)
         AOS.init();
     </script>
 </body>
+
 </html>
